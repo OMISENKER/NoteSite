85c29a337b71577d3932e66df391636ee85eb2bd
Dominique Eclavia <dominiquealvarez8888@gmail.com>
2022-08-15 17:34:37 +0800

    slide functioning

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function show() {
+var x = document.getElementById("title");
+x.addEventListener("click", move);
+var y = document.getElementsByClassName("slide");
+y.addEventListener("mouseout", moveOut);
+setTimeout(move, 2000);
 
+
+function move(){
     document.getElementById('overlayed-side-slide-bar').style.marginLeft = "-1vw";
     document.getElementById('overlayed-side-slide-bar-1').style.marginLeft = "-1vw";
     document.getElementById('overlayed-side-slide-bar-2').style.marginLeft = "-1vw";
     document.getElementById('title').style.marginLeft = "2vw";
     document.getElementById('title-1').style.marginLeft = "2vw";
     document.getElementById('title-2').style.marginLeft = "2vw";
+}
+
+function moveOut(){
+    document.getElementById('overlayed-side-slide-bar').style.marginLeft = "-35vw";
+    document.getElementById('overlayed-side-slide-bar-1').style.marginLeft = "-35vw";
+    document.getElementById('overlayed-side-slide-bar-2').style.marginLeft = "-35vw";
+    document.getElementById('title').style.marginLeft = "-32vw";
+    document.getElementById('title-1').style.marginLeft = "-32vw";
+    document.getElementById('title-2').style.marginLeft = "-32vw";
 }
