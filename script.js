20d004bbadceb98793f7903b4e7a774d194597c3
Dominique Eclavia <dominiquealvarez8888@gmail.com>
2022-08-15 16:51:30 +0800

    slide bar working v1

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function show() {
 
-    document.getElementById('overlayed-side-slide-bar').toggle('active'); 
+    document.getElementById('overlayed-side-slide-bar').style.marginLeft = "-1vw";
+    document.getElementById('overlayed-side-slide-bar-1').style.marginLeft = "-1vw";
+    document.getElementById('overlayed-side-slide-bar-2').style.marginLeft = "-1vw";
+    document.getElementById('title').style.marginLeft = "2vw";
+    document.getElementById('title-1').style.marginLeft = "2vw";
+    document.getElementById('title-2').style.marginLeft = "2vw";
 }
