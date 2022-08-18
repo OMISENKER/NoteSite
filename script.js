8038a331bf5d7f92281af7ab76e25b4c104cb98f
Omi <dominiquealvarez8888@gmail.com>
2022-08-18 21:18:27 +0800

    removed hover implemented onclick

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var x = document.getElementById("title");
-x.addEventListener("click", move);
-var y = document.getElementsByClassName("slide");
-y.addEventListener("mouseout", moveOut);
-setTimeout(move, 2000);
+var firstTitle = document.getElementById("title");
+firstTitle.addEventListener("click", move);
 
+var secondTitle = document.getElementById("title-1");
+secondTitle.addEventListener("click", moveSecondTitle);
+
+var thirdTitle = document.getElementById("title-2");
+thirdTitle.addEventListener("click", moveThirdTitle);
 
 function move(){
     document.getElementById('overlayed-side-slide-bar').style.marginLeft = "-1vw";
@@ -12,13 +14,43 @@ function move(){
     document.getElementById('title').style.marginLeft = "2vw";
     document.getElementById('title-1').style.marginLeft = "2vw";
     document.getElementById('title-2').style.marginLeft = "2vw";
+    document.getElementById('overlayed-side-slide-bar').style.zIndex = "5";
+    document.getElementById('title').style.zIndex = "5";
+    document.getElementById('overlayed-side-slide-bar-1').style.zIndex = "4";
+    document.getElementById('title-1').style.zIndex = "4";
+    document.getElementById('overlayed-side-slide-bar-2').style.zIndex = "3";
+    document.getElementById('title-2').style.zIndex = "3";
+    
 }
 
-function moveOut(){
-    document.getElementById('overlayed-side-slide-bar').style.marginLeft = "-35vw";
-    document.getElementById('overlayed-side-slide-bar-1').style.marginLeft = "-35vw";
-    document.getElementById('overlayed-side-slide-bar-2').style.marginLeft = "-35vw";
-    document.getElementById('title').style.marginLeft = "-32vw";
-    document.getElementById('title-1').style.marginLeft = "-32vw";
-    document.getElementById('title-2').style.marginLeft = "-32vw";
+function moveSecondTitle(){
+    document.getElementById('overlayed-side-slide-bar').style.marginLeft = "-1vw";
+    document.getElementById('overlayed-side-slide-bar-1').style.marginLeft = "-1vw";
+    document.getElementById('overlayed-side-slide-bar-2').style.marginLeft = "-1vw";
+    document.getElementById('title').style.marginLeft = "2vw";
+    document.getElementById('title-1').style.marginLeft = "2vw";
+    document.getElementById('title-2').style.marginLeft = "2vw";
+    document.getElementById('overlayed-side-slide-bar-1').style.zIndex = "5";
+    document.getElementById('title-1').style.zIndex = "5";
+    document.getElementById('overlayed-side-slide-bar').style.zIndex = "4";
+    document.getElementById('title').style.zIndex = "4";
+    document.getElementById('overlayed-side-slide-bar-2').style.zIndex = "3";
+    document.getElementById('title-2').style.zIndex = "3";
 }
+
+function moveThirdTitle(){
+    document.getElementById('overlayed-side-slide-bar').style.marginLeft = "-1vw";
+    document.getElementById('overlayed-side-slide-bar-1').style.marginLeft = "-1vw";
+    document.getElementById('overlayed-side-slide-bar-2').style.marginLeft = "-1vw";
+    document.getElementById('title').style.marginLeft = "2vw";
+    document.getElementById('title-1').style.marginLeft = "2vw";
+    document.getElementById('title-2').style.marginLeft = "2vw";
+    document.getElementById('overlayed-side-slide-bar-2').style.zIndex = "5";
+    document.getElementById('title-2').style.zIndex = "5";
+    document.getElementById('overlayed-side-slide-bar').style.zIndex = "4";
+    document.getElementById('title').style.zIndex = "4";
+    document.getElementById('overlayed-side-slide-bar-1').style.zIndex = "3";
+    document.getElementById('title-1').style.zIndex = "3";
+}
+
+
